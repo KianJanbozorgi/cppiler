e75d8e9fed8417bf179ceaeac27622ae9182e925
Kian <142554596+KianJanbozorgi@users.noreply.github.com>
2025-01-26 05:50:26 -0800

    added comments to parser file

--- parser_cpp.py
+++ parser_cpp.py
@@ -5,16 +5,16 @@ class Parser:
     def __init__(self, parse_table):
         self.parse_table = parse_table
     def walk(self,tokens):
-        current_token = 0
-        node = 0
+        current_token = 0 # iterating tokens
+        node = 0    #iterating tree
         parse_tree = []
         stack = ["Start"]
         while stack:
             top_stack = stack.pop()
-            if top_stack == "$":
+            if top_stack == "$": # stack getting empty meaning that PDA is in accept state
                 if current_token == len(token):  
                     return parse_tree
-            elif top_stack in self.parse_table:
+            elif top_stack in self.parse_table: # for iterating the variables
                 token = tokens[current_token][1] if current_token < len(tokens) else "$" 
                 rule = self.parse_table[top_stack].get(token)
                 if not rule:
@@ -28,19 +28,19 @@ class Parser:
                 children = rule.split()
                 for child in reversed(children):
                     if child != "ε":
-                        stack.append(child)
+                        stack.append(child) # updating stack and tree
                 for child in reversed(children):
                     parse_tree[node][1].append(child)
                 node += 1
 
-            else:
+            else: # getting a terminal or epsilon
                 token = tokens[current_token][1]  
                 if token == top_stack:  
-                    current_token += 1    
+                    current_token += 1 # moving forward in tokens    
                 elif tokens[current_token][0] == top_stack:
                     parse_tree[node-1][1].clear()
                     parse_tree[node-1][1].append(tokens[current_token][1])
-                    current_token += 1
+                    current_token += 1 # getting identifiers and numbers in tree
         return parse_tree 
 
 tokens_list = Lexer_cpp.tokens_list
